1bdf6adaeb677ce55043939eb43534c48ecbbc4f
aadilbagwan5858-pixel <aadilbagwan5858@gmail.com>
2026-07-13 20:47:18 +0530

    Integrated Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 import App from "./App";
 import "./index.css";
 
 import { ThemeProvider } from "./context/ThemeContext";
 import { CartProvider } from "./context/CartContext";
+
+// Google Analytics
+ReactGA.initialize("G-MQCJTY1W66");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname + window.location.search,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
